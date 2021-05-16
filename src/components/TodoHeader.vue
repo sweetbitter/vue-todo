c3e11740120a553d69b3179160a65d679ea06c75
hoseok <qwera0415@heartgames.co.kr>
2021-05-16 18:14:24 +0900

    vue-todo-component

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <h1>TODO vue!</h1>
+    </header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+h1{
+    color : #2F3B52;
+    Font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>
